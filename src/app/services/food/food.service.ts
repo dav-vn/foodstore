e26d417a4ef3171ab49476c3e13238cf73c1ea2e
dav-vn <vartanyandg@gmail.com>
2023-03-10 14:21:35 +0300

    Adding getAll func. into the FoodService for giving all the food images aa arr

--- src/app/services/food/food.service.ts
+++ src/app/services/food/food.service.ts
@@ -6,4 +6,15 @@ import { Injectable } from '@angular/core';
 export class FoodService {
 
   constructor() { }
+
+  getAll():String[] {
+    return [
+      '/assets/images/foods/food-1.jpg',
+      '/assets/images/foods/food-2.jpg',
+      '/assets/images/foods/food-3.jpg',
+      '/assets/images/foods/food-4.jpg',
+      '/assets/images/foods/food-5.jpg',
+      '/assets/images/foods/food-6.jpg'
+    ]
+  }
 }
